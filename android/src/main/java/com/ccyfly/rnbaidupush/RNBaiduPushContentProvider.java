8a47e9a82d137795c4ea415ecc3daa620d43925b
Calvin Chan <calvin.cy.chan@ps.asl.com.hk>
2019-06-25 12:40:40 +0800

    - update provider extends

--- android/src/main/java/com/ccyfly/rnbaidupush/RNBaiduPushContentProvider.java
+++ android/src/main/java/com/ccyfly/rnbaidupush/RNBaiduPushContentProvider.java
@@ -2,7 +2,7 @@ package com.ccyfly.rnbaidupush;
 
 import com.baidu.android.pushservice.PushInfoProvider;
 
-public class RNBaiduPushContentProvider {
+public class RNBaiduPushContentProvider extends PushInfoProvider {
     public RNBaiduPushContentProvider() {
         super();
     }
